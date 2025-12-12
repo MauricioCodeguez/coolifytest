21e2373827f67e911628eb52b05b83d71b4b2994
Mauricio Codeguez <mauricio.codeguez@gmail.com>
2025-12-11 21:18:38 -0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mauriciocodeguez/coolifytest
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World!")
+	})
+
+	fmt.Println("Server running on port", port)
+	http.ListenAndServe(":"+port, nil)
+}
